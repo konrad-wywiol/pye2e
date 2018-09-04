2bbe179cb9430b231a02f6fdfb246c18cb05e741
Konrad Wywiol <konrad.wywiol@xsolve.pl>
2018-09-04 08:23:59 +0200

    structure rework

--- tests/data/page_url_data.py
+++
@@ -1,8 +0,0 @@
-url = {
-    'homepage': '',
-    'bing': 'https://www.bing.com',
-    'yahoo': 'https://www.yahoo.com',
-    'python.org': 'https://www.python.org/',
-    'selenium.com': 'https://www.seleniumhq.org/',
-    'gherkin.com': 'https://www.gherkin.com/'
-}
